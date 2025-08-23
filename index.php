8de6da14d913f44ebad71e094804e758e39784d9
lvcaspacifico <lucasreinaldopacificoh@gmail.com>
2025-08-23 18:36:17 -0300

    add README.md

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
 
-    setcookie("fav_food", "pizza", time() + (86400 * 2), "/");
-    setcookie("fav_desert", "iceream cílios", time() + (86400 * 2), "/");
-
-    foreach($_COOKIE as $key => $value){
-        echo "Key = ".$key." | Value = ".$value."<br>";
-    }
 ?>
