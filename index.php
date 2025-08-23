7c83ed2cb1eecce8ad39e634a48f977b0bec4d7e
lvcaspacifico <lucasreinaldopacificoh@gmail.com>
2025-08-23 17:59:43 -0300

    VALIDATE e SANITIZE dos inputs

--- index.php
+++ index.php
@@ -6,36 +6,45 @@
     <title>Hello, world of PHP 🐘</title>
 </head>
 <body> 
+    <form action="index.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username">
+        <label for="age">Age:</label>
+        <input type="text" name="age">
+        <label for="email">Email:</label>
+        <input type="text" name="email">
+        <br><br>
+        <input type="submit" name="submit">
+    </form>
 </body>
 </html>
 
 <?php
     /* 
-        Functions + string functions
+        Sanitizar / Validar forms input
     */ 
-    function happy_birthday(string $name, string $friendsName){
-        $name = strtolower($name);
-        $name = trim($name);
-        $subs = substr($name, 0, 5);
-        $len = strlen($name);
-        $splitPos = strpos($name, " ");
-        $firstName = ucfirst(substr($name, 0, $splitPos));
-        $lastName = ucfirst(substr($name, ($splitPos + 1), $len));
-        $name = str_replace(" ", "", $name);
-        // explode() pra splitar e devolver array
-        // implode() pra juntar um array e devolver string
-        echo "Happy birthday, <strong>@{$name}</strong>!🥳<br>";
-        echo "Username len = {$len} | Substr = {$subs}<br>";
-        echo "Nome = {$firstName} | Sobrenome = {$lastName}<br>";
-    
-        $friendsNameParts = explode(" ", strtolower(trim($friendsName)));
-        $friendsFirstName = ucfirst($friendsNameParts[0]);
-        $friendsUsername = "@" . strtolower(implode($friendsNameParts));
-        $friendsLastName = ucfirst($friendsNameParts[1]);
-         echo "Bbem-vindo, amigo, <strong>{$friendsUsername}</strong>!👋🏻<br>";
-        echo "Nome = {$friendsFirstName} | Sobrenome = {$friendsLastName}<br>";
-    
-    } 
+     if(isset($_POST["submit"])){
+        $username = filter_input(INPUT_POST, "username", 
+        FILTER_SANITIZE_SPECIAL_CHARS
+        );
+        echo "Hello, " . $username . "!";
 
-    happy_birthday("    LVCAS PACIFICO", "    JOHN COCK");
+        // $age = filter_input(INPUT_POST, "age" ,
+        // FILTER_SANITIZE_NUMBER_INT   
+        // );
+        // echo "Age = " . $age;
+        $email = filter_input(INPUT_POST, "email" ,
+        FILTER_SANITIZE_EMAIL   
+        );
+        echo "Email = " . $email;
+
+        $age = filter_input(
+            INPUT_POST,
+            "age",
+            FILTER_VALIDATE_INT);
+        echo "Age now is = " . $age;
+        if(empty($age)){
+            echo "please fill a valid age";
+        }
+     }
 ?>
