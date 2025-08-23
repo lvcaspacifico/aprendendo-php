e0bd4688090c4f0008e1a99a7bec2fd60e7b5c70
lvcaspacifico <lucasreinaldopacificoh@gmail.com>
2025-08-23 17:48:05 -0300

    str métodos

--- index.php
+++ index.php
@@ -5,28 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello, world of PHP 🐘</title>
 </head>
-<body>
-    <form action="index.php" method="POST">
-        <input type="checkbox" name="foods[]" value="Pizza">
-        Pizza<br>
-        <input type="checkbox" name="foods[]" value="Hotdog">
-        Hotdog<br>
-        <input type="checkbox" name="foods[]" value="Tacco">
-        Tacco<br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
+<body> 
 </body>
 </html>
 
 <?php
     /* 
-        Checkbox 
-    */
-    if(isset($_POST["submit"])){
-        
-        $foods = $_POST["foods"];
-        foreach($foods as $f){
-            echo "Food = {$f}<br>";
-        }
-    }
+        Functions + string functions
+    */ 
+    function happy_birthday(string $name, string $friendsName){
+        $name = strtolower($name);
+        $name = trim($name);
+        $subs = substr($name, 0, 5);
+        $len = strlen($name);
+        $splitPos = strpos($name, " ");
+        $firstName = ucfirst(substr($name, 0, $splitPos));
+        $lastName = ucfirst(substr($name, ($splitPos + 1), $len));
+        $name = str_replace(" ", "", $name);
+        // explode() pra splitar e devolver array
+        // implode() pra juntar um array e devolver string
+        echo "Happy birthday, <strong>@{$name}</strong>!🥳<br>";
+        echo "Username len = {$len} | Substr = {$subs}<br>";
+        echo "Nome = {$firstName} | Sobrenome = {$lastName}<br>";
+    
+        $friendsNameParts = explode(" ", strtolower(trim($friendsName)));
+        $friendsFirstName = ucfirst($friendsNameParts[0]);
+        $friendsUsername = "@" . strtolower(implode($friendsNameParts));
+        $friendsLastName = ucfirst($friendsNameParts[1]);
+         echo "Bbem-vindo, amigo, <strong>{$friendsUsername}</strong>!👋🏻<br>";
+        echo "Nome = {$friendsFirstName} | Sobrenome = {$friendsLastName}<br>";
+    
+    } 
+
+    happy_birthday("    LVCAS PACIFICO", "    JOHN COCK");
 ?>
